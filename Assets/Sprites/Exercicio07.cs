2dfd688b61aff62c00a98ada4ebfd5d716f68ed4
EMERSON CAUA SOUZA RODRIGUES <52668563860@info.local>
2024-11-05 17:25:56 -0300

    Exercicios Feitos

--- Assets/Sprites/Exercicio07.cs
+++ Assets/Sprites/Exercicio07.cs
@@ -8,17 +8,28 @@ using static UnityEngine.EventSystems.EventTrigger;
 
 public class Exercicio07 : MonoBehaviour
 {
-//    (Dado com N faces) Para fazer um jogo de RPG, vamos precisar
-// calcular probabilidades e simular a rolagem de dados.Crie um
-//script em que o usuário defina o número de faces de um dado
-//    (int) e calcule a rolagem de um dado com esse número de faces.
-//Use a função abaixo para o cálculo: Random.Range (valor_min,
-//valor_max); (Coloque o código dentro da função-evento Start).
+    //    (Dado com N faces) Para fazer um jogo de RPG, vamos precisar
+    // calcular probabilidades e simular a rolagem de dados.Crie um
+    //script em que o usuário defina o número de faces de um dado
+    //    (int) e calcule a rolagem de um dado com esse número de faces.
+    //Use a função abaixo para o cálculo: Random.Range (valor_min,
+    //valor_max); (Coloque o código dentro da função-evento Start).
+
+    [SerializeField] int faces;
+
     void Start()
     {
-        
 
+        //if (faces == 0)
+        //{
+        //    print("O numero de fases do dado não pode ser: " + faces);
+        //}
+        //else
+        //{
+        //    print (Random.Range(1, faces));
+        //}
 
+        print(faces == 0 ? ("O numero de fases do dado não pode ser: " + faces) : (Random.Range(1, faces)));
         
     }
 
